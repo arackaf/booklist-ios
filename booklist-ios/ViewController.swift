9e2da4eeff4c99e03a64b6eeabb5e8278de015cf
Adam Rackis <adamrackis@hotmail.com>
2022-05-04 11:08:22 -0500

    Initial navigation

--- booklist-ios/ViewController.swift
+++ booklist-ios/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonClick(_ sender: Any) {
+        self.mainLabel.text = "Ayyyyyy";
+        
+        
+        
+        var viewControllers = self.navigationController!.viewControllers
+        _ = viewControllers.popLast()
 
+        // Push targetViewController
+        let nextViewController = self.storyboard!.instantiateViewController(withIdentifier: "loggedInMain")
+        viewControllers.append(nextViewController)
+
+        self.navigationController?.setViewControllers(viewControllers, animated: true)
+        
+        
+        // let nextViewController = storyboard?.instantiateViewController(withIdentifier: "loggedInMain")
+        // self.navigationController!.pushViewController(nextViewController, animated: true)
+        
+
+
+        
+        //self.present(nextViewController, animated: true)
+    }
+    
 }
 
