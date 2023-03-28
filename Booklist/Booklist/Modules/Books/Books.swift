8da4b4694f9defa4ca2969b13053d412853af182
Adam Rackis <adamrackis@hotmail.com>
2023-03-28 09:02:09 -0500

    Add data request

--- Booklist/Booklist/Modules/Books/Books.swift
+++ Booklist/Booklist/Modules/Books/Books.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct Books: View {
     var body: some View {
-        Text("Books")
+        Text("Books").task(priority: .background) {
+            print("a")
+            let url = URL(string: "https://mylibrary.io/api/books-public")!
+            var request = URLRequest(url: url)
+            
+            // the request is JSON
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            // the response expected to be in JSON format
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                print("Callback")
+                guard error == nil else {
+                    print("error")
+                    print(error?.localizedDescription)
+                    return
+                }
+                print(data)
+                
+            }.resume()
+        }
     }
 }
 
