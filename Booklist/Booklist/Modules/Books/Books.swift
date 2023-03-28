8ccdd16560c0447450eb1c5f17697a56ab14e49d
Adam Rackis <adamrackis@hotmail.com>
2023-03-28 09:01:49 -0500

    Tabs set up

---
+++ Booklist/Booklist/Modules/Books/Books.swift
@@ -0,0 +1,20 @@
+//
+//  Books.swift
+//  Booklist
+//
+//  Created by Adam Rackis on 3/28/23.
+//
+
+import SwiftUI
+
+struct Books: View {
+    var body: some View {
+        Text("Books")
+    }
+}
+
+struct Books_Previews: PreviewProvider {
+    static var previews: some View {
+        Books()
+    }
+}
