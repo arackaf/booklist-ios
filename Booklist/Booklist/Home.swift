afcfaf14faa3a3cb5d8ffed4d71346d60390aed3
Adam Rackis <adamrackis@hotmail.com>
2023-03-28 07:58:11 -0500

    Rename root view

--- Booklist/Booklist/Home.swift
+++ Booklist/Booklist/Home.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Home: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Home()
     }
 }
