6f9d846dc024d5fe8774a56ace3c9cf97f55a7e0
Adam Rackis <adamrackis@hotmail.com>
2023-03-28 07:57:03 -0500

    Project started

---
+++ Booklist/Booklist/BooklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooklistApp.swift
+//  Booklist
+//
+//  Created by Adam Rackis on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BooklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Booklist/BooklistUITests/BooklistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BooklistUITestsLaunchTests.swift
+//  BooklistUITests
+//
+//  Created by Adam Rackis on 3/28/23.
+//
+
+import XCTest
+
+final class BooklistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
