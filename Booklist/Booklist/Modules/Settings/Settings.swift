3aaf2d4718a814c39a75389e03760553279edafd
Adam Rackis <adamrackis@hotmail.com>
2023-03-30 19:58:25 -0500

    Settings tweak to test layout

--- Booklist/Booklist/Modules/Settings/Settings.swift
+++ Booklist/Booklist/Modules/Settings/Settings.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct Settings: View {
     var body: some View {
-        Text("Settings")
+        VStack(alignment: .leading) {
+            Text("Settings").foregroundColor(.red)
+            Image("kw-short")
+                .frame(minWidth: 10)
+        }
     }
 }
 
